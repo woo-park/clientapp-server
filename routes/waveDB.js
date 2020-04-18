c24cfe30d9dc3517ca06b98856a8ff60cd9a2c2c
woo-park <wp503@nyu.edu>
2020-04-18 21:47:50 +0900

    redux store added along with api action creators reducers fetches auth based data

--- routes/waveDB.js
+++ routes/waveDB.js
@@ -141,6 +141,10 @@ router.get('/data', function(req, res) {
       res.send(`${acc}<br\/>${my_lat_lon}`);
     });
 })
+
+
+
+
 // mini/marine/marine_buoy_cosmos.jsp?stn=22458`
 router.get('/official', function(req, res, next) {
   let areacode
@@ -149,8 +153,8 @@ router.get('/official', function(req, res, next) {
   } else {
     areacode = 22457 //default jungmun
   }
-
-  let uri = `http://www.weather.go.kr/mini/marine/marine_buoy_cosmos.jsp?stn=${22457}`
+// &dtm=-3
+  let uri = `http://www.weather.go.kr/mini/marine/marine_buoy_cosmos.jsp?stn=${22457}&tm=${yyyymmddhh()}`
   const options = {
     uri: uri,
     method: 'GET'
@@ -226,6 +230,165 @@ router.get('/official', function(req, res, next) {
       res.send(accData)
     })
   })
+})
+
+router.get('/gust', function(req, res, next) {
+  let areacode = {areacode: 22184}
+  let uri = `http://marine.kma.go.kr/custom/leisure.pop?work=beach&id=21`
+  const options = {
+    uri: uri,
+    method: 'GET'
+  }
+
+  return new Promise((resolve, reject) => {
+    request(options, function(error, response, html) {
+        if(error) {
+          console.log('err occured while requesting');
+        }
+        $ = cheerio.load(html);
+        let listRow = []
+        let dataDict = {}
+        let dataList = []
+
+
+
+        // $(element).find('table[class=forecastNew3] thead').children().last().each((i, e) => {
+        //   console.log($(e),'s')
+        //   // dataList[i] = {1:1}
+        // })
+        $('table[class=forecastNew3] thead tr th').each((i, e) => {
+
+          dataList[i] = {[$(e).text()]:null}
+        })
+        console.log(dataList)
+        // $(element).find('td').each((i, item) => {
+        //
+        //   listRow.push($(item).text())
+        // })
+
+        let table_develop = $('table[class=forecastNew3] tbody tr').each((index, element) => {
+
+
+        })
+        // console.log(listRow)
+
+        // res.send(table_develop)
+        res.send('hi')
+
+    })
+  })
+  //
+  // return new Promise((resolve, reject) => {
+  //   request(options, function(error, response, html) {
+  //     if(error) {
+  //       console.log('err occured while requesting');
+  //     }
+  //     //
+  //     // let acc
+  //     $ = cheerio.load(html);
+  //     let table_develop = $('table[class=table_develop] tbody tr').each((index, element) => {
+  //       let tdRow = $(element).find('td')
+  //
+  //       tdRow.each((i, item) => {
+  //         console.log($(item).text())
+  //       })
+  //     })
+  //     //   // console.log($(element).html())
+  //     //   let tdRow = $(element).find('td')
+  //     //   let listRow = []
+  //     //   //
+  //     //   // tdRow.each((i, item) => {
+  //     //   //   listRow.push($(item).text())
+  //     //   // })
+  //     //
+  //       // acc += listRow[1] + listRow[2] + listRow[3]
+  //
+  //     })
+  //     // res.send(acc)
+  // })
+
+
+})
+
+function yyyymmddhh() {
+    var now = new Date();
+    var y = now.getFullYear();
+    var m = now.getMonth() + 1;
+    var d = now.getDate();
+    var h = now.getHours()
+    var mm = m < 10 ? '0' + m : m;
+    var dd = d < 10 ? '0' + d : d;
+    var hh = h < 10 ? '0' + h : h;
+    return `${y}.${mm}.${dd}.${hh}`
+}
+
+console.log(yyyymmddhh())
+router.get('/official/preview', passport.authenticate('jwt', {session: false}), function(req, res, next) {
+
+  let token = getToken(req.headers);
+
+  if (token) {
+    console.log('access granted')
+
+    let areacode
+    // if (req.body) {
+    //   areacode = req.body
+    // } else {
+      areacode = {areacode: 22457}
+    // }
+
+
+    let uri = `http://www.weather.go.kr/mini/marine/marine_buoy_cosmos.jsp?stn=${areacode.areacode}&tm=${yyyymmddhh()}&dtm=-12`
+    const options = {
+      uri: uri,
+      method: 'GET'
+    }
+    let accData = 'Offset,Height,Periods,Date,Time,Temp\n'
+    return new Promise((resolve, reject) => {
+      request(options, function(error, response, html) {
+        if(error) {
+          console.log('err occured while requesting');
+        }
+
+        let rows = [] // should append here
+
+        $ = cheerio.load(html);
+        let table_develop = $('table[class=table_develop] tbody tr').each((index, element) => {
+          console.log($(element).html())
+          let tdRow = $(element).find('td')
+          let listRow = []
+
+          tdRow.each((i, item) => {
+            listRow.push($(item).text())
+          })
+            accData += tide[index % 24].toFixed(1)    // adding offset here bc time isn't neccesary now
+            accData += ','
+
+            accData += parseFloat(listRow[2]).toFixed(1).trim()
+            accData += ','
+
+            accData += parseFloat(listRow[4]).toFixed(1).trim()
+            accData += ','
+
+            accData += listRow[0].slice(1,3)    //date
+            accData += ','
+
+            accData += listRow[0].slice(6,8)  //time
+            accData += ','
+
+            accData += listRow[5].trim()      //temp
+            accData += '\n'
+
+          // console.log(listRow)
+          rows.push($(element).html())
+        });
+        // console.log($('table[class=table_develop]').html())
+        res.send(accData)
+      })
+    })
+  } else {
+    return res.status(403).send({success: false, msg: 'Unauthorized.'})
+  }
 })
 
 router.post('/official', function(req, res, next) {
