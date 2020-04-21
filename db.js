9ed981add60d3d31a21a14a4ea799f6e938e9918
Woo <leo9634@naver.com>
2020-04-20 22:38:52 -0400

    mongodb console log after connection

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mern-secure', {useNewUrlParser:true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/mern-secure', {useNewUrlParser:true, useUnifiedTopology: true}).then((data)=>{
+  console.log('mongodb connected');
+  console.log('mongodb running on mongodb://localhost:27017/mern-secure')
+  console.log(data, 'data')
+  console.log(data.models,'models');
+})
 
 mongoose.set('useFindAndModify', false);
